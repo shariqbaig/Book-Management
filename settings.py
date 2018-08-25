8c7181254f95374ec30ff8648e04544d940323e1
shariqbaig <shariqbaig97@hotmail.com>
2018-08-26 03:47:07 +0500

    -

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ import os
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\GIT Projects\\Book-Management\\database.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app/database.db'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
