be511e1a15a8a09b0c74e1605e6ee7e54cd4096e
shariqbaig <shariqbaig97@hotmail.com>
2018-08-26 02:48:57 +0500

    -

--- db_create_BookModel.py
+++ db_create_BookModel.py
@@ -2,7 +2,7 @@ from BookModel import bookDb, Book
 
 bookDb.create_all()
 
-Book.add_book('Do Androids Dream of Electric Sheep?', 7.99, 101)
+Book.add_book('Do Androids Dream of Electric Sheep', 7.99, 101)
 Book.add_book('Something Wicked This Way Comes', 10.99, 102)
 Book.add_book('The Unbearable Lightness of Being', 6.99, 103)
 
