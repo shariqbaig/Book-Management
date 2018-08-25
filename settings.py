ccd12930e42f2dc2c33b45360da1393170006542
shariqbaig <shariqbaig97@hotmail.com>
2018-08-26 01:26:17 +0500

    -

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\GIT Projects\\Book-Management\\database.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app/database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'learn'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app/database.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'learn'
